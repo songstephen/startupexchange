2eed1c074f5af424b2770d8cc4147c55b7c83ac6
Stephen Song <stephen.song@gatech.edu>
2016-01-18 13:32:32 -0500

    Contact form validations + submissions

--- js/helpers.js
+++ js/helpers.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-
+    // about page
 
     $.getJSON('data/bios.json', function(data) {
         console.log("bagel");
@@ -33,6 +33,42 @@ $(document).ready(function() {
             $(".modal").fadeToggle('slow');
         }
     });
+
+    // contact page
+
+    $(".submitted").hide();
+    $(".error-msg").hide();
+
+    $(function() {
+        if ( document.location.href.indexOf('?submitted') > -1 ) {
+            $(".submitted").show();
+            $(".signup").hide();
+        }
+    });
+
+$("#mc-embedded-subscribe").click(function() {
+    if (!$('#mce-MMERGE14').val().length) {
+            $(".error-msg").show();
+            $(".error-msg").text('Please enter your first name.');
+            return false;
+        } else if (!$('#mce-MMERGE15').val().length) {
+            $(".error-msg").show();
+            $(".error-msg").text('Please enter your last name.');
+            return false;
+        } else if (!$('#mce-EMAIL').val().length) {
+            $(".error-msg").show();
+            $(".error-msg").text('Please enter your email address.');
+            return false;
+        } else if (!(/[^\s@]+@[^\s@]+\.[^\s@]+/.test($('#mce-EMAIL').val()))) {
+            $(".error-msg").show();
+            $(".error-msg").text('Make sure your email address is valid.');
+            return false;
+        } else {
+            $(".error-msg").hide();
+            return true;
+        }
+    });
+
 });
 
 function updateModal(data, i){
@@ -41,3 +77,4 @@ function updateModal(data, i){
     $('#sx').html(data.bios[i].sx);
     $('#interests').html("Interests: " + data.bios[i].interests)
 }
+
